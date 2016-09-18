d9841ec8077cd2e764d2cc2bc69a02a15d98d7af
Manoj Kulkarni <manojkulkarni2006@gmail.com>
2016-09-19 00:05:05 +0530

    Added User validation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,6 @@
 class TransactionsController < ApplicationController
 
 	before_filter :get_users, only: [:index, :create]
-	before_filter :check_valid_users, only: [:create]
 
 	def index
 		@bill = Bill.new
@@ -11,7 +10,11 @@ class TransactionsController < ApplicationController
 		unless params[:bill].blank?
 			@bill = Bill.new(transaction_parameters)	
 			if @bill.valid?
-				flash[:notice] = 'Transaction created successfully.'
+				if are_users_valid? && user_amount_matches?
+					flash[:notice] = 'Transaction created successfully.'
+				else
+					flash[:error] = 'Please check Users and their Amounts'
+				end
 			end
 		end
 		render :index
@@ -25,6 +28,20 @@ class TransactionsController < ApplicationController
 
 	def get_users
 		@users = User.all
+	end
+
+	def are_users_valid?
+		params[:user_present].present? && params[:user_present].to_set.subset?(@users.ids.map(&:to_s).to_set)
+	end
+
+	def user_amount_matches?
+		if params[:user_present].present?
+			total_amt = 0
+			params[:user_present].each do |user_id|
+				total_amt += params["user_amt_#{user_id}".intern].to_f unless params["user_amt_#{user_id}".intern].blank?
+			end
+		end
+		params[:bill][:amount].to_f.eql?(total_amt)
 	end
 
 end
